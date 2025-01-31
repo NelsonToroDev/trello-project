11f282557310f1905f1278d2c4880e157a793aa4
Nelson Toro <nelson.toro.flores@gmail.com>
2025-01-31 16:13:37 -0400

    Implemented look and feel for boards component and include Accordion component

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBox, faWaveSquare, faClock, faAngleUp, faAngleDown, faHeart, faBorderAll, faUsers, faGear } from '@fortawesome/free-solid-svg-icons';
+import { faTrello } from '@fortawesome/free-brands-svg-icons';
+import {CdkAccordionModule} from '@angular/cdk/accordion';
+
 
 @Component({
   selector: 'app-boards',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FontAwesomeModule, CdkAccordionModule],
   templateUrl: './boards.component.html'
 })
 export class BoardsComponent {
-
+  faBox = faBox;
+  faWaveSquare = faWaveSquare;
+  faTrello = faTrello;
+  faClock = faClock;
+  faAngleUp = faAngleUp;
+  faHeart = faHeart;
+  faBorderAll = faBorderAll;
+  faUsers = faUsers;
+  faGear = faGear;
+  faAngleDown = faAngleDown;
 }
